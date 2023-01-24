3dd2aeab9dcabed747298fb6d2b7c0ba0f0e5d09
J. Nick Koston <nick@koston.org>
2023-01-23 17:00:59 -1000

    0.0.1

--- src/mopeka_iot_ble/__init__.py
+++ src/mopeka_iot_ble/__init__.py
@@ -21,7 +21,7 @@ from sensor_state_data import (
 
 from .parser import MopekaIOTBluetoothDeviceData
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 __all__ = [
     "MopekaIOTBluetoothDeviceData",
