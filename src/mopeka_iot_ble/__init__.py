d8e551c6e0c270431c4149782dd088b313e7cf2b
semantic-release <semantic-release>
2026-08-07 17:30:54 +0000

    1.0.0
    Automatically generated by python-semantic-release

--- src/mopeka_iot_ble/__init__.py
+++ src/mopeka_iot_ble/__init__.py
@@ -25,7 +25,7 @@ from sensor_state_data import (
 from .models import MediumType
 from .parser import MopekaIOTBluetoothDeviceData
 
-__version__ = "0.8.0"
+__version__ = "1.0.0"
 
 __all__ = [
     "BinarySensorDescription",
